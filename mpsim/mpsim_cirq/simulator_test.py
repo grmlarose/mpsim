bbae1bd8a1c75f8191e946733a8f1e0a6ad70397
Ryan LaRose <rlarose@x.team>
2020-04-16 12:29:44 -0700

    Adds unit test simulating a one-dimensional supremacy circuit.

--- mpsim/mpsim_cirq/simulator_test.py
+++ mpsim/mpsim_cirq/simulator_test.py
@@ -29,6 +29,7 @@ def test_simulate_bell_state_cirq_circuit():
         res.wavefunction, np.array([1., 0., 0., 1.]) / np.sqrt(2)
     )
 
+
 def test_simulate_bell_state_mpsim_circuit():
     """Tests correctness for the final MPS wavefunction when simulating a
     Cirq Circuit which prepares a Bell state.
@@ -51,3 +52,20 @@ def test_simulate_bell_state_mpsim_circuit():
     assert np.allclose(
         res.wavefunction, np.array([1., 0., 0., 1.]) / np.sqrt(2)
     )
+
+
+def test_simulate_one_dimension_supremacy_circuit():
+    """Tests simulating a one-dimensional supremacy circuit
+    using the MPSimulator.
+    """
+    # Get the circuit
+    circuit = cirq.experiments.generate_boixo_2018_supremacy_circuits_v2_grid(
+        n_rows=1, n_cols=5, cz_depth=10, seed=1
+    )
+    print(circuit)
+
+    # Do the simulation using the MPS Simulator
+    sim = MPSimulator()
+    res = sim.simulate(circuit)
+    assert isinstance(res, MPS)
+    assert np.isclose(res.norm(), 1.)
