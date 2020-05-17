985d3308654a48e96a0fd4b005da3c6f1b00dcc3
Ryan LaRose <rlarose@x.team>
2020-05-17 02:30:13 -0700

    Updates setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,20 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     long_description = f.read()
 
+with open('development_requirements.txt') as f:
+    dev_requirements = f.read().splitlines()
+
 
 requirements = [
     requirement.strip() for requirement in open("requirements.txt").readlines()
 ]
 
+description = ("A package for using matrix product states "
+               "to simulate quantum circuits.")
 
 setup(name="mpsim",
-      version="99.9.9",
-      description="SImulate (noisy) quantum circuits using matrix product states (MPS).",
+      version="0.1.0",
+      description=description,
       long_description=long_description,
       long_description_content_type="text/markdown",
       author="Ryan LaRose",
@@ -21,5 +26,9 @@ setup(name="mpsim",
       license="MIT",
       python_requires=">=3.6",
       install_requires=requirements,
+      extras_require={
+            "development": set(dev_requirements),
+            "test": dev_requirements
+      },
       packages=find_packages()
       )
